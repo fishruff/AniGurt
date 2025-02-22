6978ebb3a4d5b0efa10d581b6adbfc9299597f17
fishruff <arsershoff@gmail.com>
2025-02-23 00:49:30 +0400

    "Позвольте вас перебить" - сказал Штирлиц группе беседующих офицеров и выхватил автомат.

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -5,7 +5,7 @@ interface PlayerProps {
     return ( 
         <div className="flex justify-around mt-20">
             <iframe className=" rounded-2xl" src={urlPlayer} width="910px" height="570px" allowFullScreen></iframe>
-            <iframe className=" rounded-2xl" src="https://html5.gamedistribution.com/da854bd614dd4fcbb4a4e9da90b4ad48/?gd_sdk_referrer_url=https://www.example.com/games/{game-path}" width="320" height="570"></iframe>
+            <iframe className="rounded-2xl" src="https://www.crazygames.com/embed/blockbuster-puzzle" allow="gamepad *;"></iframe>
         </div>
      );
 }
